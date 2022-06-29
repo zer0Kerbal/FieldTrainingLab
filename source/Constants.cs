5e205679d9ec851ec1d3702f3c3962c3569186bc
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-06-28 23:11:31 -0500

    rename Source --> source :open_file_folder:

---
+++ source/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+using FieldTrainingLab;
+
+public class Constants
+{
+    public const string MODNAME = "FieldTrainingLab";
+    public const string MODTITLE = "Field Training Lab";
+    public static readonly string ROOT_PATH = KSPUtil.ApplicationRootPath;
+    public static readonly string CONFIG_BASE_FOLDER = ROOT_PATH + "GameData/";
+    public static string FTL_BASE_FOLDER { get { return CONFIG_BASE_FOLDER + MODNAME + "/"; } }
+    public static string FTL_NODENAME = MODNAME;
+    public string FTL_CFG_FILE { get { return FTL_BASE_FOLDER + "PluginData/FTL_Settings.cfg"; } }
+}

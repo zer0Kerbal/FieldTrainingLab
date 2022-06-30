eef7db2bf57ac50fdddc34bc46f4b364e0319ac8
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-06-29 23:31:41 -0500

    Update AssemblyInfo.cs

--- source/Properties/AssemblyInfo.cs
+++ source/Properties/AssemblyInfo.cs
@@ -21,20 +21,14 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Field Training Lab (FTL)")]
 [assembly: AssemblyDescription("Train Kerbals using Science Points, for Kerbal Space Program")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kerbalnaut Training Industries, Inc.")]
-[assembly: AssemblyProduct("Field Training Lab")]
+[assembly: AssemblyProduct("Field Training Lab (FTL)")]
 [assembly: AssemblyCopyright("Copyright (c) 2016 Efour (c) 2019, 2022 zer0Kerbal")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-//[assembly: AssemblyInformationalVersionAttribute("Train Kerbals using Science Points, for Kerbal Space Program")]
+[assembly: Guid("AD686AF3-DDDC-4991-909F-EAD9C16C469B")]

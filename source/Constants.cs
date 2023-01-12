a577742e9174e3becb859c2e2bd295673cf92957
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2023-01-11 23:46:30 -0600

    Update Constants.cs

--- source/Constants.cs
+++ source/Constants.cs
@@ -20,14 +20,16 @@
 using System.Text;
 
 using FieldTrainingLab;
+using KSP.Localization;
 
 public class Constants
 {
-    public const string MODNAME = "FieldTrainingLab";
-    public const string MODTITLE = "Field Training Lab";
+    public const string MODNAME = "#FTL-addon-nameFolder";		// #FTL-addon-nameFolder = FieldTrainingLab
+    public const string MODTITLE = "#FTL-addon-name";		// #FTL-addon-name = Field Training Lab
     public static readonly string ROOT_PATH = KSPUtil.ApplicationRootPath;
     public static readonly string CONFIG_BASE_FOLDER = ROOT_PATH + "GameData/";
     public static string FTL_BASE_FOLDER { get { return CONFIG_BASE_FOLDER + MODNAME + "/"; } }
     public static string FTL_NODENAME = MODNAME;
-    public string FTL_CFG_FILE { get { return FTL_BASE_FOLDER + "PluginData/FTL_Settings.cfg"; } }
+    public string FTL_CFG_FILE { get { return FTL_BASE_FOLDER + "PluginData/settings.ftl"; } }
+    //public string FTL_CFG_FILE { get { return FTL_BASE_FOLDER + "PluginData/FTL_Settings.cfg"; } }
 }

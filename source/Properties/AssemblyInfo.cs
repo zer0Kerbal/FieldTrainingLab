b380ef2a7c506c17864b7b28750a294ef0fba830
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2023-01-11 21:04:00 -0600

    Update AssemblyInfo.cs

--- source/Properties/AssemblyInfo.cs
+++ source/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿/* Field Training Lab (FTL)
  * This addon adds a training center in the science laboratory. Paying science points gets kerbals experience. For Kerbal Space Program.
  * Copyright (C) 2016 EFour
- * Copyright (C) 2019, 2022 zer0Kerbal (zer0Kerbal at hotmail dot com)
+ * Copyright (C) 2019, 2022, 2023 zer0Kerbal (zer0Kerbal at hotmail dot com)
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,8 +27,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Kerbalnaut Training Industries, Inc.")]
 [assembly: AssemblyProduct("Field Training Lab (FTL)")]
 [assembly: AssemblyCopyright("Copyright (c) 2016 Efour (c) 2019, 2022 zer0Kerbal")]
+[assembly: AssemblyTrademark("Trademark (tm) 2016 Efour (c) 2019, 2023 zer0Kerbal")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("Kerbal")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("AD686AF3-DDDC-4991-909F-EAD9C16C469B")]
